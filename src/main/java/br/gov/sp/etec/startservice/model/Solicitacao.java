3032ca061225e2ae808405e5b8fdeb3c33f0a172
ALUNO - SL103 <sl103@escolasebraesp.local>
2022-09-09 22:14:37 -0300

    Entrega do Projeto

---
+++ src/main/java/br/gov/sp/etec/startservice/model/Solicitacao.java
@@ -0,0 +1,34 @@
+package br.gov.sp.etec.startservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Solicitacao {
+	
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  
+  private String nomeSolicitacao;
+  private String tipoSolicitacao;
+  
+  
+  public String getNomeSolicitacao() {
+	return nomeSolicitacao;
+}
+public void setNomeSolicitacao(String nomeSolicitacao) {
+	this.nomeSolicitacao = nomeSolicitacao;
+}
+public String getTipoSolicitacao() {
+	return tipoSolicitacao;
+}
+public void setTipoSolicitacao(String tipoSolicitacao) {
+	this.tipoSolicitacao = tipoSolicitacao;
+}
+  
+ 
+  
+}
